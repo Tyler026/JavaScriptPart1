86c539e0a688366680611d915eb279df59423705
Tyler Fuqua <tyler@agilityda.com>
2017-12-25 17:57:15 -0700

    Hands On Project 3

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ var dividNumbers = numberOne / numberTwo;*/
 
 //  Hands On 3
 
+// This part runs the calculateGrade function and shows the result
+var inputGrade = prompt("Enter Grade");
+var parsedInt = parseInt(inputGrade);
+var finalGrade = calculateGrade(parsedInt);
+
+function calculateGrade() {
+
+}
+if (inputGrade >= 90) {
+   alert("A")
+} else if (inputGrade >= 80) {
+   alert("B")
+}  else if (inputGrade >= 70) {
+    alert("C")
+} else if (inputGrade >= 60) {
+    alert("D")
+} else {
+    alert("F");
+}
+
