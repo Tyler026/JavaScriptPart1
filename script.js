7c4466245c9af575307cbd70e6cc1e246a29095a
Tyler Fuqua <tyler@agilityda.com>
2017-12-25 17:06:55 -0700

    Hands On 3

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // alert("Welcome To My Page!");
-
-var myFavoriteNumber = 8;
+// Hands On 2
+/*var myFavoriteNumber = 8;
 var myFavoritePet = "Toby";
 var rainIsLovely = true;
 var numberOne = 24;
 var numberTwo = 57;
 var addNumber = numberOne + numberTwo;
-var dividNumbers = numberOne / numberTwo;
+var dividNumbers = numberOne / numberTwo;*/
+
+//  Hands On 3
+
